5951bf98faca3f5dfb6cee693917c8509eed4532
Yubin-Cho <74402562+yubin1219@users.noreply.github.com>
2021-05-06 19:47:36 +0900

    Update Generator.py

--- pix2pix/Generator.py
+++ pix2pix/Generator.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 
 class UnetGenerator(nn.Module):
-    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False):
+    def __init__(self, input_nc, output_nc, num_downs, ngf=64, norm_layer=nn.BatchNorm2d:
         super(UnetGenerator, self).__init__()
         unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=None, norm_layer=norm_layer, innermost=True)
         for i in range(num_downs - 5):
-            unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer, use_dropout=use_dropout)
+            unet_block = UnetSkipConnectionBlock(ngf * 8, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
         unet_block = UnetSkipConnectionBlock(ngf * 4, ngf * 8, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
         unet_block = UnetSkipConnectionBlock(ngf * 2, ngf * 4, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
         unet_block = UnetSkipConnectionBlock(ngf, ngf * 2, input_nc=None, submodule=unet_block, norm_layer=norm_layer)
@@ -19,50 +19,46 @@ class UnetGenerator(nn.Module):
 
 # Defines the submodule with skip connection.
 class UnetSkipConnectionBlock(nn.Module):
-    def __init__(self, outer_nc, inner_nc, input_nc, submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d, use_dropout=False):
+    def __init__(self, outer_nc, inner_nc, input_nc, submodule=None, outermost=False, innermost=False, norm_layer=nn.BatchNorm2d):
         super(UnetSkipConnectionBlock, self).__init__()
         if input_nc is None:
             input_nc = outer_nc
             
-        self.outermost = outermost
-        
-        downconv = nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
-        downrelu = nn.LeakyReLU(0.2, True)
-        downnorm = norm_layer(inner_nc)
-        
-        uprelu = nn.ReLU(True)
-        upnorm = norm_layer(outer_nc)    
+        self.outermost = outermost   
         
         if outermost:
-            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1)
-            down = [downconv]
-            up = [uprelu, upconv, nn.Tanh()]
-            model = down + [submodule] + up
+            self.down = nn.Sequential(nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias),
+                                      submodule)
+            self.up = nn.Sequential(nn.ReLU(True),
+                                    nn.ConvTranspose2d(inner_nc * 2, outer_nc, kernel_size=4, stride=2, padding=1),
+                                    nn.Tanh()
+                                   )
+            
         elif innermost:
-            upconv = nn.ConvTranspose2d(inner_nc, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1, bias=use_bias)
-            down = [downrelu, downconv]
-            up = [uprelu, upconv, upnorm]
-            model = down + up
+            self.down = nn.Sequential(nn.LeakyReLU(0.2, True),
+                                      nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias))
+            self.up = nn.Sequential(nn.ReLU(True),
+                                    nn.ConvTranspose2d(inner_nc, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias),
+                                    norm_layer(outer_nc)
+                                   )
+            
         else:
-            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc,
-                                        kernel_size=4, stride=2,
-                                        padding=1, bias=use_bias)
-            down = [downrelu, downconv, downnorm]
-            up = [uprelu, upconv, upnorm]
-
-            if use_dropout:
-                model = down + [submodule] + up + [nn.Dropout(0.5)]
-            else:
-                model = down + [submodule] + up
-
-        self.model = nn.Sequential(*model)
+            upconv = nn.ConvTranspose2d(inner_nc * 2, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias)
+            self.down = nn.Sequential(nn.LeakyReLU(0.2, True),
+                                      nn.Conv2d(input_nc, inner_nc, kernel_size=4, stride=2, padding=1, bias=use_bias),
+                                      norm_layer(inner_nc),
+                                      submodule)
+            self.up = nn.Sequential(nn.ReLU(True),
+                                    nn.ConvTranspose2d(inner_nc * 2, outer_nc, kernel_size=4, stride=2, padding=1, bias=use_bias),
+                                    norm_layer(outer_nc)
+                                   )
 
     def forward(self, x):
         if self.outermost:
-            return self.model(x)
+            out = self.down(x)
+            out = self.up(out)
+            return out
         else:
-            return torch.cat([x, self.model(x)], 1)
+            out = self.down(x)
+            out = self.up(out)
+            return torch.cat([x, out], 1)
